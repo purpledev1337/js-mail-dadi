6d340ff9a1d36f42cfcc4f91157059f1fdd3edc8
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-10-26 16:40:22 +0200

    dice version with data in HTML

---
+++ dadi/bonus/main.js
@@ -0,0 +1,28 @@
+// Generate a random number (from 1 to 6) for the player
+let userNumber = Math.floor(Math.random() * 6) + 1;
+console.log("Numero dell'utente:", userNumber);
+
+// Generate a random number (from 1 to 6) for the computer
+let comNumber = Math.floor(Math.random() * 6) + 1;
+console.log("Numero del computer:", comNumber);
+
+// variables to insert values in the HTML
+const playerBoard = document.getElementById("player_number");
+const computerBoard = document.getElementById("computer_number");
+const resultBoard = document.getElementById("result_board");
+
+// Print the numbers in the HTML
+playerBoard.append(userNumber);
+computerBoard.append(comNumber);
+
+// Check who has the higher number.
+// Print the winner.
+if (userNumber > comNumber) {
+    resultBoard.append("Complimenti, hai vinto!");
+} else if (comNumber > userNumber) {
+    resultBoard.append("Ritenta, sarai più fortunato!");
+} else if (comNumber = userNumber) {
+    resultBoard.append("Incredibile, è un pareggio!");
+}
+
+
