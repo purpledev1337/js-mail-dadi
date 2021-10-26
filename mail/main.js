b257c75510e39aee12638a69114c6e3b885bd92c
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-10-26 15:54:06 +0200

    working console version

--- mail/main.js
+++ mail/main.js
@@ -1,8 +1,21 @@
 // Create the email list manually
 
+const emailList = ["andrea@gmail.com", "marco@gmail.com", "luca@gmail.com", "stefano@gmail.com", "giovanni@gmail.com"];
+
 // Ask the user for the email
 
-// Check if the mail is in the created mail list
+const userEmail = prompt("Inserisci la tua email:");
 
+console.log(userEmail);
+console.log(emailList);
+
+// Check if the mail is in the created mail list
 // Print a message based on the check result
+
+for (i = 0; i < emailList.length; i++) {
+    if (userEmail == emailList[i]) {
+        console.log("La tua mail è valida!");
+    }
+}
+
 
